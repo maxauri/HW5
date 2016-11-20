224905bd2b75277170a0f376d06018fbcdc8bfeb
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-11-20 09:27:39 -0500

    if statement

---
+++ issetnull.php
@@ -0,0 +1,16 @@
+<?php
+$a = "test";
+$b = "anothertest";
+
+var_dump(isset($a));      // TRUE
+var_dump(isset($a, $b)); // TRUE
+
+unset ($a);
+
+var_dump(isset($a));     // FALSE
+var_dump(isset($a, $b)); // FALSE
+
+$foo = NULL;
+var_dump(isset($foo));   // FALSE
+
+?>
